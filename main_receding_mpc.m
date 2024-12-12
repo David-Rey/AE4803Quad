4a9b70aa53057b9a2ec764da30ae3748310ada7a
Kevin Maranto <marantokevin@gmail.com>
2024-12-12 16:02:14 -0500

    Resolve merge conflict

--- main_receding_mpc.m
+++ main_receding_mpc.m
@@ -4,7 +4,7 @@ clear; clc; close all;
 x0 = [-3, -2, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].';  % initial state
 xf = [5, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].';  % final state
 dt = 0.01;  % time step
-sim_horizon = 100;   % total sim time (iterations, i.e 50 => 0.5 seconds)
+sim_horizon = 600;   % total sim time (iterations, i.e 50 => 0.5 seconds)
 planning_horizon = 3;  % "look ahead" amount (seconds)
 n = length(x0);
 m = 4;
